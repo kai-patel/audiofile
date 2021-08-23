81873a0bdf3c08b27857676dc5bb589af8dd9d52
Kai Patel <00kaipatel@gmail.com>
2021-08-23 22:44:39 +0100

    Server side auth now returns tokens

--- server/index.js
+++ server/index.js
@@ -6,6 +6,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 const CLIENT_ID = process.env.CLIENT_ID || "";
 const CLIENT_SECRET = process.env.CLIENT_SECRET || "";
+const REDIRECT_URI = process.env.REDIRECT_URI || "http://localhost:5000/authorize";
 
 app.use(cors());
 app.use(express.json());
@@ -17,7 +18,29 @@ app.use('/', (req, res) => {
 // Handle authorization code flow with Spotify
 app.post('/authorize', (req, res) => {
     const token = req.body.token;
-    // TODO: Install and use spotify-web-api-node wrapper
+    const spotifyApi = new SpotifyWebApi({
+        clientId: CLIENT_ID,
+        clientSecret: CLIENT_SECRET,
+        redirectUri: REDIRECT_URI
+    });
+
+    spotifyApi.authorizationCodeGrant(token).then((data) => {
+        console.log(`Expires In: ${data.body.expires_in}`);
+        console.log(`Access Token: ${data.body.access_token}`);
+        console.log(`Refresh Token: ${data.body.refresh_token}`);
+        res.json({
+            accessToken: data.body.access_token,
+            refreshToken: data.body.refresh_token,
+            expiresIn: data.body.expires_in
+        });
+    }, (err) => {
+        console.log("Could not authorize!");
+        console.log(err);
+        res.json({
+            message: "Could not authorize",
+            error: err
+        });
+    });
     
 });
 
