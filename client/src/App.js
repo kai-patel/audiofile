bc883442699490a1e1498492553beae4ece11c79
Kai Patel <00kaipatel@gmail.com>
2021-08-24 21:02:24 +0100

    Formatting index.js, styling of login button (App.js)

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import react from 'react';
-import { Container, Button } from '@material-ui/core';
+import { Box, Container, Button } from '@material-ui/core';
 import axios from 'axios';
 
 const CLIENT_ID = process.env.CLIENT_ID || "";
@@ -9,7 +9,7 @@ const spotifyURI = "https://accounts.spotify.com/authorize?reponse_type=code" +
       `&client_id=${CLIENT_ID}` +
       `&scope=${encodeURIComponent(scopes)}` +
       `&redirect_uri=${encodeURIComponent(REDIRECT_URI)}`;
-      
+
 const handleLogin = async (e) => {
     e.preventDefault();
     console.log("Clicked!");
@@ -19,13 +19,11 @@ const handleLogin = async (e) => {
 
 const App = () => {
     return (
-            <div style={{"width": "100%", "height": "100%"}}>
-            <Container>
-            <Button variant="contained" color="primary" size="large" onClick={handleLogin}>
-            Login
-        </Button>
-            </Container>
-            </div>
+        <Box display="flex" justifyContent="center" alignItems="center" minHeight="100vh">
+            <Button size="large" variant="contained" color="primary" onClick={handleLogin}>
+                Login
+            </Button>
+        </Box>
     );
 }
 
