88e29b2f632707f944642287a37893f0176ec371
Kai Patel <00kaipatel@gmail.com>
2021-08-24 23:24:50 +0100

    Changed endpoint /authorize to /login to avoid callback URI conflict, added more logic to login button handler function

--- client/src/App.js
+++ client/src/App.js
@@ -4,17 +4,26 @@ import axios from 'axios';
 
 const CLIENT_ID = process.env.CLIENT_ID || "";
 const REDIRECT_URI = process.env.REDIRECT_URI || "http://localhost:5000/authorize"
+const API_URI = process.env.API_URI || "http://localhost:5000"
 const scopes = "user-read-private"
 const spotifyURI = "https://accounts.spotify.com/authorize?reponse_type=code" +
       `&client_id=${CLIENT_ID}` +
       `&scope=${encodeURIComponent(scopes)}` +
       `&redirect_uri=${encodeURIComponent(REDIRECT_URI)}`;
 
+// Handle auth -- retrieve tokens from api
 const handleLogin = async (e) => {
     e.preventDefault();
     console.log("Clicked!");
-    let response = await axios.get(spotifyURI);
-    console.log(response);
+
+    // Retrieve client code from Spotify
+    let { codeData } = await axios.get(spotifyURI);
+    console.log(codeData);
+
+    // Send client code to API, get back tokens
+    let { tokenData } = await axios.post(`${API_URI}/login`, { "code": codeData.code });
+    console.log(tokenData);
+
 }
 
 const App = () => {
