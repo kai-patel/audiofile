0350a9b1fb3b0998a39762c4fd20477a616a2565
Kai Patel <00kaipatel@gmail.com>
2021-09-15 10:43:20 +0100

    Refactoring authorization flow

--- client/src/App.js
+++ client/src/App.js
@@ -1,27 +1,21 @@
-import React from 'react';
-import { Box, Container, Button } from '@material-ui/core';
-import axios from 'axios';
+import React from "react";
+import { Box } from "@material-ui/core";
 
-const CLIENT_ID = "1a457bdb20ac4f9c81b53a9e37cb6568";
-const REDIRECT_URI = "http://localhost:5000/login"
-const API_URI = "http://localhost:5000"
-
-const scopes = "user-read-private"
-
-const spotifyURI = 'https://accounts.spotify.com/authorize' +
-      '?response_type=code' +
-      '&client_id=' + CLIENT_ID +
-      (scopes ? '&scope=' + encodeURIComponent(scopes) : '') +
-      '&redirect_uri=' + encodeURIComponent(REDIRECT_URI);
+import Login from "./components/Login/Login";
+import Home from "./components/Home/Home";
 
 const App = () => {
     return (
-            <Box display="flex" justifyContent="center" alignItems="center" minHeight="100vh">
-            <Button href={spotifyURI} size="large" variant="contained" color="primary">
-            Login
-        </Button>
-            </Box>
+        <Box
+            display="flex"
+            justifyContent="center"
+            alignItems="center"
+            minHeight="100vh"
+        >
+            <Login />
+            <Home />
+        </Box>
     );
-}
+};
 
 export default App;
---
+++ client/src/components/Login/Login.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { Button } from "@material-ui/core";
+import axios from "axios";
+
+const CLIENT_ID = "1a457bdb20ac4f9c81b53a9e37cb6568";
+const REDIRECT_URI = "http://localhost:3000";
+const API_URI = "http://localhost:5000";
+
+const scopes = "user-read-private";
+
+const spotifyURI =
+    "https://accounts.spotify.com/authorize" +
+    "?response_type=code" +
+    "&client_id=" +
+    CLIENT_ID +
+    (scopes ? "&scope=" + encodeURIComponent(scopes) : "") +
+    "&redirect_uri=" +
+    encodeURIComponent(REDIRECT_URI);
+
+const Login = () => {
+    <Button href={spotifyURI} size="large" variant="contained" color="primary">
+        Login
+    </Button>;
+};
+
+export default Login;
