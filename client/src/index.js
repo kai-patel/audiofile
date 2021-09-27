20a4b3b86fe5d4475fece6e932012b50281d1c05
Kai Patel <00kaipatel@gmail.com>
2021-09-27 12:59:43 +0100

    Install tailwind

--- client/src/index.js
+++ client/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import { ThemeProvider, createTheme } from "@material-ui/core/styles";
 import { CssBaseline } from "@material-ui/core";
+import "./index.css";
 
 const theme = createTheme({
     palette: {
