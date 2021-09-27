cffe6eb4375d0a85424c24e8e50ce904bad5410b
Kai Patel <00kaipatel@gmail.com>
2021-09-27 12:53:43 +0100

    Replace list of songs with table

--- client/src/components/Songs/Songs.js
+++ client/src/components/Songs/Songs.js
@@ -6,28 +6,41 @@ import {
     Paper,
     Typography,
     Divider,
+    TableContainer,
+    Table,
+    TableHead,
+    TableRow,
+    TableCell,
+    TableBody,
 } from "@material-ui/core";
 
 const Songs = ({ songs }) => {
-    const elems = songs.map(({ track }, index) => (
-        <React.Fragment key={index}>
-            <ListItem>
-                <ListItemText primary={track.name} />
-            </ListItem>
-            <Divider variant="fullWidth" component="li" />
-        </React.Fragment>
+    const table = songs.map(({ track }, index) => (
+        <TableRow key={index}>
+            <TableCell>{track.name}</TableCell>
+            <TableCell>{track.artists[0].name}</TableCell>
+        </TableRow>
     ));
 
     return (
-        <Paper square variant="outlined">
-            <Typography align="center" variant="h6">
-                Songs
-            </Typography>
-            <Divider variant="fullWidth" component="ul" />
-            <List dense={true} style={{ maxHeight: "100vh", overflow: "auto" }}>
-                {elems}
-            </List>
-        </Paper>
+        <TableContainer
+            component={Paper}
+            style={{ maxHeight: "75vh", maxWidth: "100%", overflow: "auto" }}
+        >
+            <Table padding="normal">
+                <TableHead>
+                    <TableRow>
+                        <TableCell align="left" style={{ width: "10vw" }}>
+                            <b>Track</b>
+                        </TableCell>
+                        <TableCell align="left" style={{ width: "20vw" }}>
+                            Artist
+                        </TableCell>
+                    </TableRow>
+                </TableHead>
+                <TableBody>{table}</TableBody>
+            </Table>
+        </TableContainer>
     );
 };
 
