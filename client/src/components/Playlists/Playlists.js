a3db623410ba16c4007d0496dacfd10a1a773b6d
Kai Patel <00kaipatel@gmail.com>
2021-09-30 16:27:21 +0100

    Refactor Playlists component with tailwind styling

--- client/src/components/Playlists/Playlists.js
+++ client/src/components/Playlists/Playlists.js
@@ -12,20 +12,31 @@ import {
 } from "@material-ui/core";
 
 const Playlists = ({ playlists, handleCheckboxChange }) => {
+    // const elems = playlists.map((playlist) => (
+    //     <ListItem key={playlist.name}>
+    //         <ListItemAvatar>
+    //             <Avatar variant="square" src={playlist?.images[1]?.url} />
+    //         </ListItemAvatar>
+    //         <ListItemText primary={playlist.name} />
+    //         <Checkbox edge="end" onChange={handleCheckboxChange(playlist.id)} />
+    //     </ListItem>
+    // ));
+
     const elems = playlists.map((playlist) => (
-        <ListItem key={playlist.name}>
-            <ListItemAvatar>
-                <Avatar variant="square" src={playlist?.images[1]?.url} />
-            </ListItemAvatar>
-            <ListItemText primary={playlist.name} />
-            <Checkbox edge="end" onChange={handleCheckboxChange(playlist.id)} />
-        </ListItem>
+        <li className="flex flex-row justify-start items-center h-10 mx-auto my-auto">
+            <img className="h-10" src={playlist?.images[0]?.url} alt="" />
+            <h1>{playlist.name}</h1>
+            <input
+                type="checkbox"
+                onChange={handleCheckboxChange(playlist.id)}
+            />
+        </li>
     ));
 
     return (
-        <div className="container grid grid-cols-1 mx-auto my-auto">
+        <div className="grid grid-cols-1 mx-auto my-auto">
             <h1>Playlists</h1>
-            {elems}
+            <ul>{elems}</ul>
         </div>
     );
 };
