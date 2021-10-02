240cd6534ae5b38587825bbf062c03b802b64edb
Kai Patel <00kaipatel@gmail.com>
2021-10-02 19:49:42 +0100

    Convert Songs component to use native HTML table

--- client/src/components/Songs/Songs.js
+++ client/src/components/Songs/Songs.js
@@ -1,46 +1,23 @@
 import React from "react";
-import {
-    List,
-    ListItem,
-    ListItemText,
-    Paper,
-    Typography,
-    Divider,
-    TableContainer,
-    Table,
-    TableHead,
-    TableRow,
-    TableCell,
-    TableBody,
-} from "@material-ui/core";
 
 const Songs = ({ songs }) => {
     const table = songs.map(({ track }, index) => (
-        <TableRow key={index}>
-            <TableCell>{track.name}</TableCell>
-            <TableCell>{track.artists[0].name}</TableCell>
-        </TableRow>
+        <tr key={index}>
+            <td>{track.name}</td>
+            <td>{track.artists[0].name}</td>
+        </tr>
     ));
 
     return (
-        <TableContainer
-            component={Paper}
-            style={{ maxHeight: "75vh", maxWidth: "100%", overflow: "auto" }}
-        >
-            <Table padding="normal">
-                <TableHead>
-                    <TableRow>
-                        <TableCell align="left" style={{ width: "10vw" }}>
-                            <b>Track</b>
-                        </TableCell>
-                        <TableCell align="left" style={{ width: "20vw" }}>
-                            Artist
-                        </TableCell>
-                    </TableRow>
-                </TableHead>
-                <TableBody>{table}</TableBody>
-            </Table>
-        </TableContainer>
+        <table>
+            <thead>
+                <tr>
+                    <th>Track</th>
+                    <th>Artist</th>
+                </tr>
+            </thead>
+            <tbody>{table}</tbody>
+        </table>
     );
 };
 
