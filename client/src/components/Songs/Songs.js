66f233b297612e847e7744f16b0883a425310178
Kai Patel <00kaipatel@gmail.com>
2021-10-02 20:15:32 +0100

    Update table styling

--- client/src/components/Songs/Songs.js
+++ client/src/components/Songs/Songs.js
@@ -3,17 +3,23 @@ import React from "react";
 const Songs = ({ songs }) => {
     const table = songs.map(({ track }, index) => (
         <tr key={index}>
-            <td>{track.name}</td>
-            <td>{track.artists[0].name}</td>
+            <td className="border border-gray-800 text-center">{track.name}</td>
+            <td className="border border-gray-800 text-center">
+                {track.artists[0].name}
+            </td>
         </tr>
     ));
 
     return (
-        <table>
+        <table className="bg-gray-300 shadow-inner border border-gray-700 p-4">
             <thead>
                 <tr>
-                    <th>Track</th>
-                    <th>Artist</th>
+                    <th className="border border-gray-800 text-center bg-green-500">
+                        Track
+                    </th>
+                    <th className="border border-gray-800 text-center bg-green-300">
+                        Artist
+                    </th>
                 </tr>
             </thead>
             <tbody>{table}</tbody>
