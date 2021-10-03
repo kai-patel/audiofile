ecf3c6d2db698b370be3adfe844fe5da2f7d7a30
Kai Patel <00kaipatel@gmail.com>
2021-10-03 14:54:27 +0100

    Remove dead code

--- client/src/components/Playlists/Playlists.js
+++ client/src/components/Playlists/Playlists.js
@@ -1,16 +1,6 @@
 import React, { useEffect } from "react";
 
 const Playlists = ({ playlists, handleCheckboxChange }) => {
-    // const elems = playlists.map((playlist) => (
-    //     <ListItem key={playlist.name}>
-    //         <ListItemAvatar>
-    //             <Avatar variant="square" src={playlist?.images[1]?.url} />
-    //         </ListItemAvatar>
-    //         <ListItemText primary={playlist.name} />
-    //         <Checkbox edge="end" onChange={handleCheckboxChange(playlist.id)} />
-    //     </ListItem>
-    // ));
-
     const elems = playlists.map((playlist, index) => (
         <li className="flex flex-row justify-between items-center h-10 mx-0 my-4" key={index}>
             <img className="h-10" src={playlist?.images[0]?.url} alt="" />
