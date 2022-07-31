2add641e01e19c5aa5d2048a1b79cf6cc1063d17
Kai Patel <00kaipatel@gmail.com>
2022-07-31 22:26:06 +0100

    Migration/breaking fixes b/c package updates

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,15 +1,13 @@
 module.exports = {
-    purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
-    darkMode: false, // or 'media' or 'class'
+	content: [
+	"./src/**/*.{js,jsx,ts,tsx}",
+	],
     theme: {
         extend: {
             fontFamily: {
                 sans: ["Roboto", "Helvetica", "Arial", "sans-serif"],
             },
         },
-    },
-    variants: {
-        extend: {},
     },
     plugins: [],
 };
