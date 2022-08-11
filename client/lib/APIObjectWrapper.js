2463d37e3702f996528d28a139e5f6b30c357b16
Kai Patel <00kaipatel@gmail.com>
2022-08-11 20:55:22 +0100

    Import SWAPINode package for API

--- client/lib/APIObjectWrapper.js
+++ client/lib/APIObjectWrapper.js
@@ -1,3 +1,5 @@
+import SpotifyWebApi from "spotify-web-api-node";
+
 const CLIENT_ID = process.env.CLIENT_ID || "";
 const CLIENT_SECRET = process.env.CLIENT_SECRET || "";
 const REDIRECT_URI = process.env.REDIRECT_URI || "";
