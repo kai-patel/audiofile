d9ac517addea17eeaa165c399aeed3804b429504
Kai Patel <00kaipatel@gmail.com>
2022-08-11 20:25:43 +0100

    Update top padding for display name

--- client/pages/Welcome.js
+++ client/pages/Welcome.js
@@ -1,6 +1,6 @@
 export function Welcome({ displayName }) {
     return (
-        <div className="flex h-[5%] w-[90%] place-self-center place-items-center place-content-center bg-green-500 shadow-lg rounded-lg">
+        <div className="flex h-[5%] w-[90%] place-self-center place-items-center mt-2 place-content-center bg-green-500 shadow-lg rounded-lg">
             <h1 className="text-center font-bold ">Welcome {displayName}</h1>
         </div>
     );
